31af85c959bd110749c6d0edb43e24212f4d34de
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/UnboundListView.Module/Duplicate.vb
@@ -0,0 +1,43 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp.DC
+
+Namespace UnboundListView.Module
+    <DomainComponent> _
+    Public Class Duplicate
+        <Browsable(False)> _
+        Public Id As Integer
+        Private privateTitle As String
+        Public Property Title() As String
+            Get
+                Return privateTitle
+            End Get
+            Set(ByVal value As String)
+                privateTitle = value
+            End Set
+        End Property
+        Private privateCount As Integer
+        Public Property Count() As Integer
+            Get
+                Return privateCount
+            End Get
+            Set(ByVal value As Integer)
+                privateCount = value
+            End Set
+        End Property
+    End Class
+    <DomainComponent> _
+    Public Class DuplicatesList
+        Private duplicates_ As BindingList(Of Duplicate)
+        Public Sub New()
+            duplicates_ = New BindingList(Of Duplicate)()
+        End Sub
+        Public ReadOnly Property Duplicates() As BindingList(Of Duplicate)
+            Get
+                Return duplicates_
+            End Get
+        End Property
+    End Class
+End Namespace
