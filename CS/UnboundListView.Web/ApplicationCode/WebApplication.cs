82d198156757a72e03a9101f562402c08eca315c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/UnboundListView.Web/ApplicationCode/WebApplication.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+
+namespace UnboundListView.Web {
+    public partial class UnboundListViewAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private UnboundListView.Module.UnboundListViewModule module3;        
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
+        private DevExpress.ExpressApp.Validation.ValidationModule module5;
+
+        public UnboundListViewAspNetApplication() {
+            InitializeComponent();
+        }
+
+        private void UnboundListViewAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new UnboundListView.Module.UnboundListViewModule();            
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // module1
+            // 
+            this.module1.AdditionalBusinessClasses.Add(typeof(DevExpress.Xpo.XPObjectType));
+            // 
+            // module5
+            // 
+            this.module5.AllowValidationDetailsAccess = true;
+            // 
+            // UnboundListViewAspNetApplication
+            // 
+            this.ApplicationName = "UnboundListView";
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.module3);            
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.securityModule1);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.UnboundListViewAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
